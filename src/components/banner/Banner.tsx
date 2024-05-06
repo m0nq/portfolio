50a4e1974a6066c9ee1e53a2e8180819db79ef69
Monk <forgetmenaught@gmail.com>
2024-05-06 13:56:44 -0700

    Added about section content.

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,12 +1,24 @@
-import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export const Banner = () => (
     <>
         <div className="banner-image">
             <StaticImage src="../../images/macbook-color.webp" alt="An illuminated MacBook laptop" />
         </div>
-        <div className="container">Container</div>
+        <div className="banner-container">
+            <div className="banner-content">
+                <h1>Monk Wellington<span style={{ color: 'pink' }}>.</span></h1>
+                <h2>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis cum ex laboriosam
+                    nam obcaecati placeat reprehenderit tenetur. Aliquid consequatur exercitationem fuga ipsum iure.
+                    Alias amet aut laborum magni temporibus?
+                </h2>
+                <div className="banner-btns">
+                    <button className="banner-button">Get in touch!</button>
+                </div>
+            </div>
+        </div>
         <div className="gradient">Gradient</div>
     </>
 );
