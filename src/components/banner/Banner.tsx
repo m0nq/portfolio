21fc42a1e524d9a0487ab9760480d7ddfbf45a0d
Monk <forgetmenaught@gmail.com>
2024-05-06 12:55:50 -0700

    Added image banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,8 +1,11 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 export const Banner = () => (
     <>
-        <div className="banner-image">Banner image</div>
+        <div className="banner-image">
+            <StaticImage src="../../images/macbook-color.webp" alt="An illuminated MacBook laptop" />
+        </div>
         <div className="container">Container</div>
         <div className="gradient">Gradient</div>
     </>
