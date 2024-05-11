ecd163c8982cd3641226e22219b847f93847914f
Monk <forgetmenaught@gmail.com>
2024-05-11 08:38:17 -0700

    Clarified about copy for clarity

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,53 +7,55 @@ export const About = () => {
                 <div>
                     <p>
                         Monk started his career at <a href="https://www.industriallogic.com/" target="_blank">Industrial
-                        Logic</a>, where he learned the ropes of Extreme Programming and how to build software that's
+                        Logic</a>, where
+                        he learned the ropes of Extreme Programming and how to build software that's
                         robust and maintainable with agile methodologies. Working with <a href="https://www.ford.com/"
-                        target="_blank">Ford Motors</a> he had the opportunity to collaborate across multiple teams,
-                        contributing to the development of a high-functioning, user-focused website that streamlined the
-                        process for customers to find the right car configuration.
+                        target="_blank">Ford Motors</a> he had the opportunity to collaborate across multiple teams.
+                        There, he contributed to the development of a high-functioning, user-focused website that
+                        streamlined the process for customers to find the right car configuration.
                         <br />
                         <br />
                         At <a href="https://reflexion.com/" target="_blank">RefleXion Medical</a>, he dedicated several
                         years to creating an intuitive Electron based user interface that allowed doctors to interact
-                        seamlessly with cutting-edge cancer treatment and research technology. This experience deepened
+                        seamlessly with cutting-edge cancer treatment and research tech. This experience deepened
                         his appreciation for the transformative power of technology.
                     </p>
                     <br />
                     <p>
                         Growing up as a dancer, Monk has always had a passion for the creative process and its ability
                         to inspire both intellectual and emotional growth. An advocate for self-empowerment, he believes
-                        that anyone,given the right tools, opportunities, and support, can cultivate their own unique
+                        that anyone – given the right tools, opportunities, and support – can cultivate their own unique
                         creative spark. Web technology tools offer a particularly powerful canvas for making connections
                         and bringing innovative ideas to life.
                         <br />
                         <br />
                         Throughout his experiences, he's cultivated a versatile skill set that allows him to approach
-                        projects with a user-centric mindset to that goes from empathy to code. This dedication to
-                        exceeding expectations is fueled by a keen eye for detail.
+                        projects with a user-centric mindset, translating empathy to code. This dedication to
+                        exceeding expectations is fueled by a desire to merge a keen eye for detail with creative
+                        intuition.
                     </p>
                     <br />
                     <p>
-                        He is open to projects that utilizes his expertise of JavaScript and React frameworks like
-                        Next.js and Gatsby
+                        He is open to projects that utilize his expertise of JavaScript and React frameworks like
+                        Next.js and Gatsby for creating single/multi page websites and enterprise applications.
                     </p>
                     <br />
                     <ul>
                         <li>
                             <span className="strong-emphasis">Small Businesses and Non-Profits:</span> Monk is
                             passionate about supporting organizations with a positive social impact. He believes his
-                            skills can help them achieve their goals and missions.
+                            skill set can help them achieve their goals and missions.
                         </li>
                         <li>
                             <span className="strong-emphasis">Creative Industries:</span> Having a background in
                             creative fields himself, Monk understands the needs of artists and creators. He's eager to
-                            use his skills to develop tools and platforms that help them showcase their work and connect
-                            with audiences.
+                            create opportunities for collaboration, developing tools and platforms that will showcase
+                            their work and help them connect with audiences.
                         </li>
                         <li>
                             <span className="strong-emphasis">Green Tech Companies:</span> Monk is environmentally
-                            conscious and wants to contribute to a more sustainable future. He's keen to work on
-                            projects that develop climate-focused solutions with these companies.
+                            conscious and seeks to contribute to a more sustainable future. He's keen to work on
+                            projects that develop climate-focused solutions with these forward thinking companies.
                         </li>
                     </ul>
                 </div>
