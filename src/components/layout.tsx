c7c7d134acd3b4a716749ae94996303d894158c4
Monk <forgetmenaught@gmail.com>
2024-05-13 12:53:02 -0700

    Revert module system - not working with TS

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,15 +5,13 @@ import { Footer } from '@components/footer/footer';
 import { useSiteMetadata } from '@hooks/use-site-metadata';
 import { Contact } from '@components/contact/Contact';
 
-import styles from './layout.module.css';
-
 export const Layout = ({ children }) => {
     const { menuLinks } = useSiteMetadata();
 
     return (
         <>
-            <div className={styles ? styles.outerWrapper : 'outer-wrapper'}>
-                <div className={styles ? styles.innerContaner : 'inner-container'}>
+            <div className="outer-wrapper">
+                <div className="inner-container">
                     <Header menuLinks={menuLinks} />
                     {children}
                     <Footer />
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -6,13 +6,11 @@ import { Skills } from '@components/skills/Skills';
 import { Projects } from '@components/projects/Projects';
 import { About } from '@components/about/About';
 
-import styles from './main.module.css';
-
 export const Main = () => {
     return (
-        <main className={styles ? styles.mainWrapper : 'main-wrapper'}>
-            <div className={styles ? styles.itemListWrapper : 'item-list-wrapper'}>
-                <div className={styles ? styles.itemList : 'item-list'}>
+        <main className="main-wrapper">
+            <div className="item-list-wrapper">
+                <div className="item-list">
                     <Section classes="banner-section">
                         <Banner />
                     </Section>
