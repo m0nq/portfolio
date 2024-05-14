60ac66992013c6a2a475efa889aac44bc7e3320b
Monk <forgetmenaught@gmail.com>
2024-05-14 13:03:02 -0700

    Added test for Main component

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -3,14 +3,14 @@ import { DetailedHTMLProps } from 'react';
 import { HTMLAttributes } from 'react';
 import { ReactNode } from 'react';
 
-export const Section = ({ styles = {}, classes = '', children }: {
+export const Section = ({ styles = {}, classes = '', children, ...props }: {
     styles?: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>,
     classes: string,
     children: ReactNode
 }) => {
     return (
         <>
-            <section style={styles} className={classes}>{children}</section>
+            <section style={styles} className={classes} {...props}>{children}</section>
         </>
     );
 };
