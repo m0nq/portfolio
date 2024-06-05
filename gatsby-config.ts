2a5108c20ea72f308c0ac087da5bded339b758e5
Monk <forgetmenaught@gmail.com>
2024-06-05 10:45:44 -0700

    Last of updated files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -90,6 +90,37 @@ const config: GatsbyConfig = {
                     families: ['Quicksand:300,400,500,600,700', 'Montserrat:100,200,300,400,500,600,700,800,900']
                 }
             }
+        // },
+        // {
+        //     resolve: 'gatsby-source-wordpress',
+        //     options: {
+        //         url: `${process.env.WORDPRESS_URL || 'http://wp-portfolio.local'}/graphql`,
+        //         verbose: true,
+        //         production: {
+        //             hardCacheMediaFiles: true,
+        //             allow404Images: true
+        //         },
+        //         develop: {
+        //             //caches media files outside of Gatsby's default cache, allowing them to persist through a cache reset.
+        //             hardCacheMediaFiles: true,
+        //             hardCacheData: true
+        //         },
+        //         html: {
+        //             useGatsbyImage: true
+        //         },
+        //         debug: {
+        //             graphql: {
+        //                 writeQueriesToDisk: true
+        //             }
+        //         }
+        //         // This is when your posts grow to more than 50 or 100.
+        //         // type: {
+        //         //     Post: {
+        //         //         // We don't need more than 50 posts in development, or 5000 in production
+        //         //         limit: process.env.NODE_ENV === 'development' ? 50 : 5000
+        //         //     }
+        //         // },
+        //     }
         }
     ].filter(Boolean) as Array<PluginRef>
 };
