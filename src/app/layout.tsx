3117bd74afcdff7b110c14d56a19ee37fdd29891
Monk <forgetmenaught@gmail.com>
2024-06-05 18:23:20 -0700

    Removed Scroll context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,29 @@
 import type { Metadata } from 'next';
+import { ReactNode } from 'react';
 import './globals.css';
 
 import { Header } from '@components/header/Header';
 import { Footer } from '@components/footer/Footer';
 import { Contact } from '@components/contact/Contact';
-import { type MenuLink } from '@data-types/menu-link.type';
-
-const menuLinks: MenuLink[] = [
-    {
-        name: 'home',
-        link: '/'
-    },
-    {
-        name: 'contact',
-        link: '/contact'
-    },
-    {
-        name: 'projects',
-        link: '/projects'
-    },
-    {
-        name: 'blog',
-        link: '/blog'
-    }
-];
+import { menuLinks } from '@data-types/menu-link.type';
 
 export const metadata: Metadata = {
     title: 'Monk Wellington',
     description: 'Monk Wellington is a software engineer based in the San Francisco Bay Area.'
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <html lang="en">
-            <body>
-                <div className="outer-wrapper">
-                    <div className="inner-container">
-                        <Header menuLinks={menuLinks} />
-                        {children}
-                        <Footer />
-                    </div>
+const RootLayout = ({ children }: Readonly<{ children: ReactNode; }>) => (
+    <html lang="en">
+        <body>
+            <div className="outer-wrapper">
+                <div className="inner-container">
+                    <Header menuLinks={menuLinks} />
+                    {children}
+                    <Footer />
                 </div>
-                <Contact />
-            </body>
-        </html>
-    );
-}
+            </div>
+            <Contact />
+        </body>
+    </html>
+);
+export default RootLayout;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,15 @@
 'use client';
 import { useContext } from 'react';
-import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
 import { capitalizeFirstLetter } from '@utils/capitalizeFirstLetter';
 import { MenuLink } from '@data-types/menu-link.type';
 import { ContactContext } from '@contexts/Contact.context';
-import { ScrollContext } from '@contexts/Scroll.context';
 import { UniversalLink } from '@components/utils/UniversalLink';
 import smilingMonkImage from '@public/smiling_monk.png';
 
 export const Header = ({ menuLinks }: { menuLinks: MenuLink[] }): JSX.Element => {
-    const pathname: string = usePathname();
     const { setIsOpen } = useContext(ContactContext);
-    const { handleScroll } = useContext(ScrollContext);
 
     const filteredMenuLinks = menuLinks
         .filter((link: MenuLink) => link.name !== 'home')
@@ -32,15 +28,9 @@ export const Header = ({ menuLinks }: { menuLinks: MenuLink[] }): JSX.Element =>
                     {filteredMenuLinks.map(({ name, link }) => {
                         return name.includes('projects') ? (
                             <li key={name}>
-                                {pathname?.endsWith('/') ? (
-                                    <button className="nav-link" onClick={handleScroll}>
-                                        {capitalizeFirstLetter(name)}
-                                    </button>
-                                ) : (
-                                    <UniversalLink to="/#projects" activeClassName="nav-link">
-                                        {capitalizeFirstLetter(name)}
-                                    </UniversalLink>
-                                )}
+                                <UniversalLink to="/#projects" activeClassName="nav-link">
+                                    {capitalizeFirstLetter(name)}
+                                </UniversalLink>
                             </li>
                         ) : (
                             <li key={name}>
