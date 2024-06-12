1a9336cf63eaf8b59f1a98ae813705c0471d06f1
Monk <forgetmenaught@gmail.com>
2024-06-12 09:05:58 -0700

    Attempted removing output export in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: process.env.GITHUB_ACTION ? 'export' : undefined,
+  // output: process.env.GITHUB_ACTION ? 'export' : undefined,
+  // output: 'export',
   images: {
     unoptimized: true
   },
