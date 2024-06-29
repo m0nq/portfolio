387819526949109a7da8987b3ec149528dfecd24
Monk <forgetmenaught@gmail.com>
2024-06-28 17:13:03 -0700

    Updated copy for offerings section

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -2,26 +2,26 @@ export const Services = () => {
     return (
         <div className="about-container">
             <div>
+                <h2 className="text-3xl strong-emphasis">Offerings</h2>
+                <br />
                 <p>
                     Reach out to me if you feel like you need:
                 </p>
                 <br />
-                <br />
                 <ul>
                     <li>
-                        <span className="strong-emphasis">An Informational Site:</span> A great looking site
-                        put up quickly that showcases your information and you likely won&apos;t need to update it
-                        often.
+                        <span className="strong-emphasis">An Informational Site:</span> Minimal turn-around time. A
+                        great looking site that showcases your information and services.
                     </li>
                     <li>
-                        <span className="strong-emphasis">A Customized Site:</span> A site with a few more moving parts
-                        to it than just conveying basic information (e-commerce site, animations, unique layouts, tool
-                        or db integrations, etc.)
+                        <span className="strong-emphasis">A Customized Site:</span> A site with a few more moving parts,
+                        customizable to your unique needs. (e-commerce site, animations, unique layouts, tool or db
+                        integrations, etc.)
                     </li>
                     <li>
-                        <span className="strong-emphasis">Monthly Site Maintenance:</span> A site (whether from scratch
-                        or already made) that needs ongoing management and maintenance of a dedicated developer for
-                        continuous optimization and bug fixes
+                        <span className="strong-emphasis">Monthly Site Maintenance:</span> Ongoing management and
+                        maintenance by a dedicated developer for continuous optimization and bug fixes for your site.
+                        This is also offered as an add-on service for my site building packages.
                     </li>
                     <li>
                         <span className="strong-emphasis">Non-Profit Projects:</span> I also offer discounted prices for
