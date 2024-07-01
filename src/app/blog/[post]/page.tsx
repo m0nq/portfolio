c2c404b3930f56a009c184b58e457de8f6489be2
Monk <forgetmenaught@gmail.com>
2024-07-01 00:43:39 -0700

    Updated post styles for small screens

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -21,7 +21,7 @@ const BlogPost = async ({ params: { post: postUri } }: { params: { post: string 
     return (
         <>
             {post.featuredImage && (
-                <Section className="relative p-7 mx-auto max-w-5xl">
+                <Section className="relative p-7 mx-auto">
                     <Image src={post.featuredImage.node.sourceUrl}
                         alt={post.featuredImage.node.altText}
                         width={900}
@@ -29,7 +29,7 @@ const BlogPost = async ({ params: { post: postUri } }: { params: { post: string 
                         className="rounded-3xl mx-auto" />
                 </Section>
             )}
-            <Section className={post.featuredImage ? 'banner' : 'py-10 mx-auto'}>
+            <Section className="banner">
                 <Article title={post.title}>
                     <div>
                         <div dangerouslySetInnerHTML={{ __html: post.content || '' }} />
