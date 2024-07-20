fbab4920bcf0238fdf2c128bf6b1479cbc5c8cf6
Monk <forgetmenaught@gmail.com>
2024-07-19 18:55:52 -0700

    Updated offerings section to be more business focused

--- src/components/offerings/offerings.tsx
+++ src/components/offerings/offerings.tsx
@@ -7,23 +7,31 @@ export const Offerings = () => {
                 <h2 className="strong-emphasis">Services</h2>
                 <br />
                 <p>
-                    I make websites for creatives and professionals who need:
+                    I help businesses and creative professionals with their websites so they can focus on the work they
+                    love.
                 </p>
+                <br />
+                <p>I can help you with:</p>
                 <br />
                 <ul>
                     <li>
                         <span className="strong-emphasis">An Informational Site:</span> Minimal turn-around time. A
-                        great looking site that showcases your information and services
+                        great looking site that showcases your information and services.
                     </li>
                     <li>
                         <span className="strong-emphasis">A Customized Site:</span> A site with a few more moving parts,
                         customizable to your needs. (e-commerce, animations, unique layouts, api/tool/db integrations,
-                        etc.)
+                        etc.).
                     </li>
                     <li>
                         <span className="strong-emphasis">Monthly Site Maintenance:</span> Ongoing management and
                         maintenance by a dedicated developer for continuous optimization and bug fixes. This is also
-                        offered as an additional service for a customized site
+                        offered as an additional service for a customized site.
+                    </li>
+                    <li>
+                        <span className="strong-emphasis">SEO Optimization / Digital Marketing:</span> Ongoing Search
+                        Engine Optimization (SEO) management. Offered also as an additional service for a customized
+                        site.
                     </li>
                     <li>
                         <span className="strong-emphasis">Non-Profit Projects:</span> Discounted prices are offered for
