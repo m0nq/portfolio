b2860ed8d8b95074a20d13b1c73a02cd435cdd7f
Monk <forgetmenaught@gmail.com>
2024-07-19 21:25:50 -0700

    Updated offerings section to elaborate on services provided

--- src/components/offerings/offerings.tsx
+++ src/components/offerings/offerings.tsx
@@ -1,43 +1,58 @@
+import Link from 'next/link';
+
 import './offerings.styles.css';
 
 export const Offerings = () => {
     return (
         <div className="offerings-container">
             <div>
-                <h2 className="strong-emphasis">Services</h2>
-                <br />
                 <p>
-                    I help businesses and creative professionals with their websites so they can focus on the work they
-                    love.
+                    I create websites for businesses and creative professionals so they can focus on what they love.
+                    Whether you need a clean and informative site or a dynamic platform with bells and whistles, I
+                    can help you bring your vision to life.
                 </p>
                 <br />
-                <p>I can help you with:</p>
+                <p>Here&apos;s what I offer:</p>
                 <br />
                 <ul>
                     <li>
-                        <span className="strong-emphasis">An Informational Site:</span> Minimal turn-around time. A
-                        great looking site that showcases your information and services.
+                        <span className="strong-emphasis">Simple & Effective Websites:</span> Looking for a quick and
+                        cost-effective solution? Let&apos;s leverage the power of no-code builders to create a beautiful
+                        and functional website showcasing your brand.
                     </li>
                     <li>
-                        <span className="strong-emphasis">A Customized Site:</span> A site with a few more moving parts,
-                        customizable to your needs. (e-commerce, animations, unique layouts, api/tool/db integrations,
-                        etc.).
+                        <span className="strong-emphasis">Customized with Power:</span> Need something more complex? My
+                        expertise extends to custom layouts, animations, e-commerce functionalities, API integrations,
+                        and database connections.
                     </li>
                     <li>
-                        <span className="strong-emphasis">Monthly Site Maintenance:</span> Ongoing management and
-                        maintenance by a dedicated developer for continuous optimization and bug fixes. This is also
-                        offered as an additional service for a customized site.
+                        <span className="strong-emphasis">Always Up and Running:</span> Focus on what you do best while
+                        I take care of your website&apos;s ongoing health. Choose from flexible monthly maintenance
+                        plans to keep your site secure, updated, and performing optimally.
                     </li>
                     <li>
-                        <span className="strong-emphasis">SEO Optimization / Digital Marketing:</span> Ongoing Search
-                        Engine Optimization (SEO) management. Offered also as an additional service for a customized
-                        site.
+                        <span className="strong-emphasis">Get Found Online:</span> Enhance your website&apos;s
+                        visibility with strategic SEO and Post-SEO optimization techniques, helping potential customers
+                        discover your brand.
                     </li>
                     <li>
-                        <span className="strong-emphasis">Non-Profit Projects:</span> Discounted prices are offered for
-                        non-profit and projects with social impact
+                        <span className="strong-emphasis">Spread the Impact:</span> Non-profit organizations and social
+                        impact projects hold a special place in my heart. I offer special discounts to support your
+                        mission and amplify your reach.
                     </li>
                 </ul>
+                <br />
+                <h2>Let&apos;s chat!</h2>
+                <br />
+                <p>
+                    I believe websites should be more than just online brochures. They should be powerful tools to
+                    connect with your audience and drive results. Let&apos;s discuss your specific needs and create a
+                    site that truly represents your brand and drives your goals.
+                </p>
+                <br />
+                <p>
+                    P.S. Check out my <span><Link href="/#projects">portfolio</Link></span> to see examples of my work!
+                </p>
             </div>
         </div>
     );
